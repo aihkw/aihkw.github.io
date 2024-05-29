54b9b2260c61098621de5a78fa4dc3b867196c61
Aiko <167198259+aihkw@users.noreply.github.com>
2024-05-30 04:39:05 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const element = document.querySelector('.pane');
+const tiltDeg = 15;
+
+function tiltpane() {
+  element.addEventListener('mousemove', ({
+    clientX,
+    clientY
+  }) => {
+    const bcr = element.getBoundingClientRect();
+    const rotX = ((clientY - bcr.top) / bcr.height * 2 - 1) * tiltDeg;
+    const rotY = ((clientX - bcr.left) / bcr.width * 2 - 1) * -tiltDeg;
+    element.firstElementChild.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg)`;
+  });
+  element.addEventListener('mouseleave', () => {
+    element.firstElementChild.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("ihtml");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("ihtml");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
---
+++ js/roexec.js
@@ -0,0 +1,63 @@
+const parent = document.getElementById('roexecs');
+fetch('/api/roexec.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach(item => {
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+        let title = document.createElement('a');
+        title.innerHTML = item.name;
+        title.href = item.url
+        title.classList.add('title');
+        let logo = document.createElement('img');
+        logo.src = item.logo
+        logo.classList.add('logo');
+        let platforms = document.createElement('div');
+        platforms.classList.add('platforms');
+        item.platforms.forEach(e => {
+            let f = document.createElement('img');
+            if (e === 'Windows' || e === 'Windows!') {
+                f.src = '/assets/windows.webp';
+            }
+            if (e === 'Mac' || e === 'Mac!') {
+                f.src = '/assets/iOS.webp';
+            }
+            if (e === 'Android' || e === 'Android!') {
+                f.src = '/assets/android.webp';
+            }
+            if (e === 'iOS' || e === 'iOS!') {
+                f.src = '/assets/iOS.webp';
+            }
+
+            if (e.slice(-1) === '!') {
+                f.style.filter = 'brightness(0.1)'
+            }
+            platforms.appendChild(f);
+        });
+        let desc = document.createElement('a');
+        desc.innerHTML = item.desc;
+        desc.classList.add('desc');
+        let status = document.createElement('a');
+        status.innerHTML = item.status;
+        status.classList.add('status');
+        if (item.status === 'Offline' || item.status === 'Discontinued') {
+            grid.style.borderBottom = '#000 30px inset';
+        }
+        if (item.status === 'Operational') {
+            grid.style.borderBottom = '#23A55A 30px inset';
+        }
+        if (item.status === 'Fixing') {
+            grid.style.borderBottom = '30px inset rgb(240 178 50 / 80%)';
+        }
+        if (item.status === 'Down') {
+            grid.style.borderBottom = '30px inset rgb(242 63 67 / 80%)';
+        }
+
+        grid.appendChild(title);
+        grid.appendChild(logo);
+        grid.appendChild(platforms);
+        grid.appendChild(desc);
+        grid.appendChild(status);
+        parent.appendChild(grid);
+    })
+})
