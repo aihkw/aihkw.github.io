8af069710b60bb1c2f3ba51569a3dda9148706c6
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-06-01 13:14:46 +0800

    3D Controls

--- js/3dviewer.js
+++ js/3dviewer.js
@@ -17,20 +17,20 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 5, 11);
+camera.position.set(4, 8, 11);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
 controls.minDistance = 5;
 controls.maxDistance = 20;
-controls.minPolarAngle = 0.5;
+controls.minPolarAngle = 0;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
-const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+const groundGeometry = new THREE.PlaneGeometry(100, 100, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
   color: 0x555555,
@@ -40,7 +40,7 @@ var groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
-var spotLight = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5);
+var spotLight = new THREE.SpotLight(0xffffff, 3, 100, .2, 1);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
@@ -76,8 +76,8 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-var select_model = document.querySelector('#select_model');
-select_model.onchange = function (e) {
+// Events
+document.querySelector('#select_model').onchange = function (e) {
   let _model_ = document.querySelector('#select_model').value;
   scene.remove(mesh);
   loader = new GLTFLoader().setPath(`/3dassets/${_model_}/`);
@@ -108,6 +108,31 @@ select_model.onchange = function (e) {
     console.error(error);
   });
 }
+var controls_pan_checked = false;
+document.querySelector('#controls_pan_reset').onclick = function () {
+  controls.target = new THREE.Vector3(0, 1, 0);
+  camera.position.set(4, 8, 11);
+}
+var controls_auto_spin = false;
+document.querySelector('#controls_auto_spin').onclick = function () {
+  controls_auto_spin = !controls_auto_spin;
+  controls.autoRotate = controls_auto_spin;
+  if (controls_auto_spin) {
+    document.querySelector('#controls_auto_spin').innerHTML = '🟢';
+  } else {
+    document.querySelector('#controls_auto_spin').innerHTML = '🔴';
+  }
+}
+document.querySelector('#spotlight_color').oninput = function () {
+  let hex = parseInt(document.querySelector('#spotlight_color').value.replace(/^#/, ''), 16);
+  spotLight.color = new THREE.Color(hex);
+}
+document.querySelector('#spotlight_angle').oninput = function () {
+  spotLight.angle = document.querySelector('#spotlight_angle').value/100
+}
+document.querySelector('#spotlight_intensity').oninput = function() {
+  spotLight.intensity = document.querySelector('#spotlight_intensity').value/10
+}
 
 let init = setInterval(()=>{
   try {
@@ -125,5 +150,21 @@ let init = setInterval(()=>{
     document.querySelector('.controller').style.display = 'none';
   }
 }, 100)
+
+document.addEventListener("keydown", function onEvent(event) {
+  controls.enablePan = event.shiftKey;
+  controls_pan_checked = event.shiftKey;
+  if (event.shiftKey) {
+    document.querySelector('#controls_pan_status').innerHTML = '✔️';
+  }
+  
+});
+document.addEventListener("keyup", function onEvent(event) {
+  controls.enablePan = event.shiftKey;
+  controls_pan_checked = event.shiftKey;
+  if (!event.shiftKey) {
+    document.querySelector('#controls_pan_status').innerHTML = '✖️'
+  }
+});
     
 animate();
